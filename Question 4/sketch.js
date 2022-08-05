22b9b4217261f7142c9777db1a9f402be80daf59
Gareth Howells <gazzzmo@gmail.com>
2022-08-05 20:43:37 +1000

    all spinning now

--- Question 4/sketch.js
+++ Question 4/sketch.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(500,500);
   background("grey");
 }
-
+angle0 = -90
 angle1 = 270
 angle2 = 90
 function draw() {
@@ -34,26 +34,27 @@ function draw() {
   // Begin drawing
   // Draw big white half circle on right
   fill("white")
-  arc(x,y,width,height,0,360);
+  arc(x,y,width,height,angle0,angle0+180);
   // Draw big black half circle on left
   fill("black")
-  arc(x,y,width,height,90,270);
+  arc(x,y,width,height,angle0+180,angle0+360);
 
   ellipseMode(CENTER);
   // Draw medium black circle at bottom
-  //fill("black")
-  //ellipse(x+shiftxW,y+shiftyW,width/2,height/2);
+  fill("black")
+  ellipse(x+shiftxW,y+shiftyW,width/2,height/2);
   // Draw medium white circle at top
-  //fill("white")
-  //ellipse(x+shiftxB,y+shiftyB,width/2,height/2);
+  fill("white")
+  ellipse(x+shiftxB,y+shiftyB,width/2,height/2);
   // Draw small white circle at bottom
-  //fill ("white")
-  //ellipse(x+shiftxW,y+shiftyW,width/8,height/8);
+  fill ("white")
+  ellipse(x+shiftxW,y+shiftyW,width/8,height/8);
   // Draw small black circle at tome
-  //fill("black")
-  //ellipse(x+shiftxB,y+shiftyB,width/8,height/8);
+  fill("black")
+  ellipse(x+shiftxB,y+shiftyB,width/8,height/8);
 
   // Rotate it!
+  angle0++
   angle1++;
   angle2++;
 
