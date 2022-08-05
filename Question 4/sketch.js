e3c68eb5da156a3b178b181d81e6b4ac0491553e
Gareth Howells <gazzzmo@gmail.com>
2022-08-05 20:34:13 +1000

    working on the big circle rotation

--- Question 4/sketch.js
+++ Question 4/sketch.js
@@ -24,27 +24,36 @@ function draw() {
   ellipseMode(CENTER);
   noStroke();
 
+  // Setup variables for rotation
   var shiftxB = r/2 *cos(angle1);
   var shiftyB = r/2 *sin(angle1);
 
   var shiftxW = r/2 *cos(angle2);
   var shiftyW = r/2 *sin(angle2);
 
+  // Begin drawing
+  // Draw big white half circle on right
   fill("white")
   arc(x,y,width,height,0,360);
+  // Draw big black half circle on left
   fill("black")
   arc(x,y,width,height,90,270);
+
   ellipseMode(CENTER);
-  ellipse(x+shiftxW,y+shiftyW,width/2,height/2);
-  fill("white")
-  ellipse(x+shiftxB,y+shiftyB,width/2,height/2);
-  ellipse(x+shiftxW,y+shiftyW,width/8,height/8);
+  // Draw medium black circle at bottom
+  //fill("black")
+  //ellipse(x+shiftxW,y+shiftyW,width/2,height/2);
+  // Draw medium white circle at top
+  //fill("white")
+  //ellipse(x+shiftxB,y+shiftyB,width/2,height/2);
+  // Draw small white circle at bottom
+  //fill ("white")
+  //ellipse(x+shiftxW,y+shiftyW,width/8,height/8);
+  // Draw small black circle at tome
+  //fill("black")
+  //ellipse(x+shiftxB,y+shiftyB,width/8,height/8);
 
-
-  fill("black")
-  ellipse(x+shiftxB,y+shiftyB,width/8,height/8);
-
-
+  // Rotate it!
   angle1++;
   angle2++;
 
